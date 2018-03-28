c29551cd99a7554597d0fecedf444df9bb3bfca5
eyi1 <ej7729@gmail.com>
2018-03-28 17:46:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,12 @@
 def my_collect (languages)
-  
+  if block_given?
+
+    i = 0
+    while i<languages.length
+      yield (languages[i])
+      i=i+1
+    end
+    languages[i].upcase
+end
+languages
 end
